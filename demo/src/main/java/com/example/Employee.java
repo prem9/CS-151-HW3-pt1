d696bad9e0e0e67913573dac9150af852276c698
Prem Agarwal <prem.m.agarwal@gmail.com>
2022-10-15 12:42:00 -0700

    finsihed

--- demo/src/main/java/com/example/Employee.java
+++ demo/src/main/java/com/example/Employee.java
@@ -1,9 +1,6 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
+
 class TooManyHoursWorkedException extends Exception{
     TooManyHoursWorkedException(String s){
         super(s);
--- demo/src/main/java/com/example/EmployeeTest.java
+++ demo/src/main/java/com/example/EmployeeTest.java
@@ -1,9 +1,6 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
+
 public class EmployeeTest 
 {
     
